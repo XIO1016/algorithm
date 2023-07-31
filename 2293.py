d1d6e7fae92b7e663d37b8b0f36553f4e4c6513c
xio1016 <carpediemsong7@naver.com>
2023-07-31 23:08:49 +0900

    2023-07-31

---
+++ 2293.py
@@ -0,0 +1,13 @@
+import sys
+
+input = sys.stdin.readline
+
+n, k = map(int, input().split())
+
+arr = [int(input()) for _ in range(n)]
+dp = [0] * (k + 1)
+dp[0] = 1
+for c in arr:
+    for i in range(c, k + 1):
+        dp[i] += dp[i - c]
+print(dp[k])
