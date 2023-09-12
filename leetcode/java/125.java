657d7edca5312816eaee0ea6756b853e3939c04f
Xio1016 <carpediemsong7@naver.com>
2023-09-12 13:46:04 +0900

    2023-09-12

---
+++ leetcode/java/125.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        int start = 0;
+        int end = s.length() -1;
+        while (start<=end){
+            char currFirst =s.charAt(start);
+            char currEnd = s.charAt(end);
+            if(!Character.isLetterOrDigit(currFirst)) start++;
+            else if(!Character.isLetterOrDigit(currEnd)) end--;
+            else{
+                if(Character.toLowerCase(currFirst)!= Character.toLowerCase(currEnd)){
+                    return false;
+                }
+                start++;
+                end--;
+            }
+        }
+        return true;
+    }
+}
